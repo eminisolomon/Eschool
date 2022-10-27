649e546638d1ccdcbf0c7f5dfac9bfd909f0e4cb
Solomon Olatunji <realsolomon07@gmail.com>
2022-10-27 22:34:54 +0100

    school and admin schema

---
+++ models/Admin.js
@@ -0,0 +1,62 @@
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+
+const validator = require("validator");
+
+const Schema = mongoose.Schema;
+
+const AdminSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "You must provide a name"],
+    },
+    username: {
+      type: String,
+      required: [true, "You must provide a username"],
+      unique: [true, "This username already exists"],
+    },
+    email: {
+      type: String,
+      unique: true,
+      validate: {
+        validator: validator.isEmail,
+        message: "Please provide a valid email address",
+      },
+      select: false,
+    },
+    role: {
+      type: String,
+      default: "admin",
+    },
+    profilePic: {
+      public_id: { type: String, required: true },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+    password: {
+      type: String,
+      required: [true, "You must provide a password!"],
+      select: false,
+    },
+  },
+  { timestamps: true }
+);
+
+AdminSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+
+AdminSchema.methods.comparePassword = async function (userPassword) {
+  const isMatch = await bcrypt.compare(userPassword, this.password);
+  return isMatch;
+};
+
+let Admin = mongoose.model("Adnin", AdminSchema);
+
+module.exports = Admin;
---
+++ models/School.js
@@ -0,0 +1,94 @@
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+
+const validator = require("validator");
+
+const Schema = mongoose.Schema;
+
+const SchoolSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "You must provide a name"],
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      validate: {
+        validator: validator.isEmail,
+        message: "Please provide a valid email address",
+      },
+      select: false,
+    },
+    description: {
+        type: String,
+    },
+    moto: {
+        type: String,
+        required: true,
+    },
+    phone: {
+      type: Number,
+      unique: [true, "This phone number already exists"],
+      select: false,
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+    lg: {
+      type: Array,
+      required: true,
+    },
+    ownership: {
+        type: String,
+        default: "Private",
+        enum: ["Private", "Public"],
+    },
+    type: {
+      type: String,
+      default: "Nursery",
+      enum: ["Primary", "Nursery", "Secondary"],
+    },
+    mgt_no: {
+        type: Number,
+        required: true,
+    },
+    owner_name: {
+        type: String,
+        required: true,
+    },
+    has_activated: {
+      type: Boolean,
+      default: false,
+    },
+    logo: {
+      public_id: { type: String, required: true },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+SchoolSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+
+SchoolSchema.methods.comparePassword = async function (userPassword) {
+  const isMatch = await bcrypt.compare(userPassword, this.password);
+  return isMatch;
+};
+
+let School = mongoose.model("Adnin", SchoolSchema);
+
+module.exports = School;
